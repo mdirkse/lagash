6c47cfada2e3da2d04ba8fe53e9b4e0f53cb80d1
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-05-22 09:31:44 +0200

    Change uptime invocation on shell startup so that it doesn't query the store

--- modules/terminal/home-manager.nix
+++ modules/terminal/home-manager.nix
@@ -1,5 +1,6 @@
 { inputs, lib, config, pkgs, ... }: {
   # Fish config
+  home.file."bin/uptime-procps".source = "${pkgs.procps}/bin/uptime";
   xdg.configFile."fish/functions/aws-creds.fish".source = ./resources/fish/functions/aws-creds.fish;
   xdg.configFile."fish/functions/fish_greeting.fish".source = ./resources/fish/functions/fish_greeting.fish;
   xdg.configFile."fish/functions/git-update-current-commit.fish".source = ./resources/fish/functions/git-update-current-commit.fish;
--- modules/terminal/resources/fish/functions/fish_greeting.fish
+++ modules/terminal/resources/fish/functions/fish_greeting.fish
@@ -1,6 +1,5 @@
 function fish_greeting
     set_color $fish_color_autosuggestion
-    command -s uptime >/dev/null
-    and eval (nix derivation show nixpkgs#procps | jq -r .[].outputs.out.path)/bin/uptime -p
+    command $HOME/bin/uptime-procps -p
     set_color normal
 end
