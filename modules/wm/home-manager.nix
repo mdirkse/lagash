900197e0b63812ecfe19a50cbe8b65ef3a7f9167
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-05-22 11:02:43 +0200

    Add default applications

--- modules/wm/home-manager.nix
+++ modules/wm/home-manager.nix
@@ -1,10 +1,4 @@
 { inputs, lib, config, pkgs, ... }: {
-  xdg.configFile."fish/functions/screenshot.fish".source =
-    ./resources/screenshot.fish;
-  xdg.configFile."sway/config".source = ./resources/sway.config;
-  xdg.configFile."waybar/config".source = ./resources/waybar/config;
-  xdg.configFile."waybar/style.css".source = ./resources/waybar/style.css;
-
   home.file.".icons/OpenZone_Black".source =
     "${pkgs.openzone-cursors}/share/icons/OpenZone_Black";
 
@@ -13,4 +7,33 @@
     cursorTheme = { name = "OpenZone_Black"; };
     theme = { name = "Pop"; };
   };
+
+  xdg.configFile."fish/functions/screenshot.fish".source =
+    ./resources/screenshot.fish;
+  xdg.configFile."sway/config".source = ./resources/sway.config;
+  xdg.configFile."waybar/config".source = ./resources/waybar/config;
+  xdg.configFile."waybar/style.css".source = ./resources/waybar/style.css;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = ["google-chrome.desktop"];
+      "text/html" = ["google-chrome.desktop"];
+      "x-scheme-handler/about" = ["google-chrome.desktop"];
+      "x-scheme-handler/http" = ["google-chrome.desktop"];
+      "x-scheme-handler/https" = ["google-chrome.desktop"];
+      "x-scheme-handler/mailto" = ["google-chrome.desktop"];
+      "x-scheme-handler/unknown" = ["google-chrome.desktop"];
+      "x-scheme-handler/webcal" = ["google-chrome.desktop"];
+    };
+  };
 }
+
+
+
+
+
+
+
+
+
