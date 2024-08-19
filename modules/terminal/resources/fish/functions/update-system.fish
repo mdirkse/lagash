b78677eb178e5a4104283e9b2c93ee3f08cdf780
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-08-19 23:17:07 +0200

    Return to the dir we were at after finishing the update

--- modules/terminal/resources/fish/functions/update-system.fish
+++ modules/terminal/resources/fish/functions/update-system.fish
@@ -1,8 +1,10 @@
 function update-system
+    pushd
     cd /home/maarten/Source/lagash/
     sudo nix-channel --update
     nix flake update
     sudo nixos-rebuild switch --flake .#laptop --upgrade
     sudo nix-collect-garbage -d
     rustup update
+    popd
 end
