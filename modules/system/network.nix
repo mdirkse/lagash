41ac04dbb3c355f0457bce7ce88075f9f3afac70
Maarten Dirkse <maarten.dirkse@gmail.com>
2025-08-27 21:18:12 +0200

    Automatically shut down wifi when connected to ethernet

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -9,4 +9,20 @@
 
   services.tailscale.enable = true;
   systemd.services."tailscaled".wantedBy = lib.mkForce [ ];
+
+  networking.networkmanager.dispatcherScripts = [{
+    source = pkgs.writeShellScript "wifi-ethernet-switch" ''
+      NM_CLI="${pkgs.networkmanager}/bin/nmcli"
+      IFACE="$1"
+      STATUS="$2"
+
+      if [ "$IFACE" = "enp58s0u1u2u1c2" ]; then
+        if [ "$STATUS" = "up" ]; then
+            $NM_CLI radio wifi off
+        elif [ "$STATUS" = "down" ]; then
+            $NM_CLI radio wifi on
+        fi
+      fi
+    '';
+  }];
 }
