cffbc4f60046f0a8b5a20074e3a56dcef35aa4cb
Maarten Dirkse <maarten.dirkse@gmail.com>
2026-03-16 22:54:17 +0100

    Add work vm

--- modules/apps/home-manager.nix
+++ modules/apps/home-manager.nix
@@ -1,5 +1,6 @@
 { inputs, lib, config, pkgs, ... }: {
   home.file."bin/winvm".source = ./resources/bin/winvm;
+  home.file."bin/jumbovm".source = ./resources/bin/jumbovm;
 
   # Chrome apps
   home.file."bin/outlook".source = ./resources/bin/outlook;
